a1a9b18db892a4652ad50844ce91837bc6fe6de1
Jason Cooley <jason_cooley@discovery.com>
2022-12-12 20:10:30 +0000

    day5

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+#![feature(iter_array_chunks)]
+
 use std::io;
 mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() {
     println!("Enter day:");
@@ -19,6 +22,7 @@ fn main() {
         "2" => day2::day2(),
         "3" => day3::day3(),
         "4" => day4::day4(),
+        "5" => day5::day5(),
         _ => println!("Invalid Day"),
     }
 }
