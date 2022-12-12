a8b23007f6e4a87a93740c6ea4ba600a4b78c815
Cooley <jcool841@yahoo.com>
2022-12-12 02:56:11 +0000

    day 4

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io;
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 fn main() {
     println!("Enter day:");
@@ -17,6 +18,7 @@ fn main() {
         "1" => day1::day1(),
         "2" => day2::day2(),
         "3" => day3::day3(),
+        "4" => day4::day4(),
         _ => println!("Invalid Day"),
     }
 }
